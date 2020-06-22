c9d9d1a948ab223e271fa557779b17f1d93771ef
tristan-tsl <tanshilinmail@gmail.com>
2020-06-22 11:04:36 +0800

    添加清理原来输入框中文字的功能, 添加日志功能

--- test.py
+++ test.py
@@ -1,36 +1,40 @@
 import ctypes
+import logging
 import time
 
 import pyautogui
 from pywinauto.application import Application
 
+logging.basicConfig(level='INFO')
 str_file_path = r"C:\Program Files\FileZilla FTP Client\filezilla.exe"
 
 
 def wait_locate_center_on_screen(image_file_path):
     """
     为图像识别增加等待功能
-    :param region:
     :param image_file_path: 图片文件路径
     :return:
     """
     find_count = 0
     while True:
+        """
+        confidence: 图片匹配度
+        """
         check_update_box_pos = pyautogui.locateCenterOnScreen(image_file_path, confidence=0.8)
         if check_update_box_pos:
             return check_update_box_pos
-        print("识别中, 当前次数为" + str(find_count))
+        logging.info("识别中, 当前次数为" + str(find_count))
         find_count = find_count + 1
         time.sleep(0.5)
 
 
 def fix_input_method_mode():
-    print("fix_input_method_mode?")
+    logging.info("fix_input_method_mode?")
     pyautogui.write('a', interval=0.25)
     if condition_input_method_mode():
         pyautogui.press(['shift'])
     pyautogui.press(['backspace'])
-    print("fix_input_method_mode.")
+    logging.info("fix_input_method_mode.")
 
 
 def condition_input_method_mode():
@@ -55,17 +59,22 @@ pyautogui.wait_locate_center_on_screen = wait_locate_center_on_screen
 
 
 def do_something(service_code, service_name):
-    print("%s框出现进行中" % service_name)
+    logging.info("%s框出现进行中" % service_name)
     something_box_check_pos = pyautogui.wait_locate_center_on_screen(
         r'image_location_resources/%s.png' % service_code)
-    print("%s框检查出现" % service_name)
-    print(something_box_check_pos)
+    logging.info("%s框检查出现" % service_name)
+    logging.info(something_box_check_pos)
     something_box_check_pos_x, something_box_check_pos_y = something_box_check_pos
     something_box_check_pos_x = something_box_check_pos_x + 60
     pyautogui.click(something_box_check_pos_x, something_box_check_pos_y)  # 点击输入框
-    print("%s输入框点击" % service_name)
+    logging.info("%s输入框点击" % service_name)
+    # 清理输入框中的文字
+    logging.info("清理%s输入框中的文字进行中" % service_name)
+    pyautogui.hotkey('ctrl', 'a')
+    pyautogui.press(['backspace'])
+    logging.info("清理%s输入框中的文字" % service_name)
     pyautogui.write(test_data[service_code], interval=0.25)  # 输入
-    print("%s输入框输入" % service_name)
+    logging.info("%s输入框输入" % service_name)
 
 
 def test():
